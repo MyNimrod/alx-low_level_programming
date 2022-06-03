d0a342f9f45deb81fef48244521abd34b59e89e2
MyNimrod <behailugobeziefentaw@gmail.com>
2022-06-03 13:43:29 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num = 48;
+
+while (num <= 57)
+{
+putchar(num);
+num++;
+}
+putchar('\n');
+return (0);
+}
