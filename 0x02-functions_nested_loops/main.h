be2f8ebd795516d8a8b9ba0416bf8e8a6bb667d3
MyNimrod <behailugobeziefentaw@gmail.com>
2022-06-07 16:24:44 -0700

    98 Battery Street, the OG98 Battery Street, the OG

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,6 +12,7 @@
  * jack_bauer - prints every minute of the day 
  * times_table - prints the 9 times table, starting with 0.
  * add - adds two integers and returns the result.
+ * print_to_98 - prints all natural numbers from n to 98
  *
  */
 
@@ -26,3 +27,4 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
+void print_to_98(int n);
