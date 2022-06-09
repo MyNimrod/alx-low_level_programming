15bab74a34ba018017c1d57107505c8059675aa5
MyNimrod <behailugobeziefentaw@gmail.com>
2022-06-09 00:20:50 -0700

    Leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,10 +11,16 @@
  */
 void print_remaining_days(int month, int day, int year)
 {
+int a = 0;
 if ((year % 4 == 0 && !(year % 100 == 0)) || (year % 400 == 0))
 {
+
+if (month >= 2 && day >= 60)
+{
+a = 1;
+}
 printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 366 - day);
+printf("Remaining days: %d\n", 366 - day - a);
 }
 else
 {
