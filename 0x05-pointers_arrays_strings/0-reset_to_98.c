9ae0fb7c20c82bf3a2b4da32b29f60cfbb95de0b
MyNimrod <behailugobeziefentaw@gmail.com>
2022-06-13 12:07:31 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - set value of the passed param to 98
+ * @n: the integer for the pointer
+ * Description: takes a pointer and updates the value to 98
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+
+/* _putchar - prints the character to the stdout
+ * reset_to_98 - takes a pointer to an int as parameter and updates the value
+ *
+ */
+
+
+
+int _putchar(char);
+void reset_to_98(int *n);
