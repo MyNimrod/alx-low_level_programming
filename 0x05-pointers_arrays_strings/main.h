0e3189a621c5e69afb610abb41c062f8138f6efd
MyNimrod <behailugobeziefentaw@gmail.com>
2022-06-13 12:20:46 -0700

    Don't swap horses in crossing a stream

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,10 +3,11 @@
 
 /* _putchar - prints the character to the stdout
  * reset_to_98 - takes a pointer to an int as parameter and updates the value
- *
+ * swap_int - swaps the value of two integers 
  */
 
 
 
 int _putchar(char);
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
