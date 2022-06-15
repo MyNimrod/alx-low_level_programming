384edf5472a37d08cfe7068b0bd894babcb5d48e
MyNimrod <behailugobeziefentaw@gmail.com>
2022-06-15 12:40:39 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+##0x06-pointers_arrays_strings
+_strcat --> concatenates two strings.
+
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#include <unistd.h>
+
+/**
+ * *_strcat - concatenates two strings.
+ *
+ *
+ */
+
+char *_strcat(char *dest, char *src);
