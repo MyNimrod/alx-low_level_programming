01ee9e06f1880922efb37ec4422d19facec09827
MyNimrod <behailugobeziefentaw@gmail.com>
2022-06-15 22:16:08 -0700

    strcmp

--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -2,4 +2,5 @@
 _strcat --> concatenates two strings.
 _strncat --> similar to _strcat but uses n bytes
 _strncpy --> Copies a string 
+_strcmp --> compares two strings
 
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,8 +4,10 @@
  * _strcat - concatenates two strings.
  * _strncat - Similar to _strcat except it uses n bytes 
  * _strncpy - Copies a string 
+ * _strcmp - a function that compares two strings
  */
 
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
