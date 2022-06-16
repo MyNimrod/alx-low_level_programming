e3da710ee435a6ce10edbba52d7ab79287eca662
MyNimrod <behailugobeziefentaw@gmail.com>
2022-06-16 06:24:47 -0700

    Numbers have Life

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,61 +1,25 @@
 #include "main.h"
-
 /**
- * exponent - prints power
- * @x: the base
- * @y: power
- * Description: prints x^y
- * Return: X^y
- */
-
-int exponent(int x, int y)
-{
-	int power;
-
-	power = x;
-
-	if (x == 0)
-		return (0);
-	if (y == 0)
-		return (1);
-	while (y >= 2)
-	{
-		power  = power * x;
-		y--;
-	}
-	return (power);
-}
-
-/**
- * print_number - prints an integer
+ * print_number - prints integer
  * @n:  the integer to be printed
  * Description:takes inputs and prints an integer
  * Return: Nothing
  */
 void print_number(int n)
 {
-	int size, digit, counter, sign;
-
-	sign = 1;
-	digit = 0;
-	size = 1;
-	counter = n;
+	int divisor = 1, i, resp;
 
 	if (n < 0)
+	{
 		_putchar('-');
-		sign = -1;
-	for (; counter >= 10 || counter <= -10; size++)
-	{
-		counter = counter / 10;
+		n *= -1;
 	}
-	counter = n;
 
-	while (size >= 2)
+	for (i = 0; n / divisor > 9; i++, divisor *= 10)
+		;
+	for (; divisor >= 1; n %= divisor, divisor /= 10)
 	{
-		digit = (counter / exponent(10, size - 1)) * sign;
-		_putchar(digit + '0');
-		counter = counter % exponent(10, size - 1);
-		size--;
+		resp = n / divisor;
+		_putchar('0' + resp);
 	}
-	_putchar(sign * counter % 10 + '0');
 }
