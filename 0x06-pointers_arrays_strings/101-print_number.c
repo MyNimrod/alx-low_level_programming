6a236cbe029c4c8e29255d2156d703dfa5311f60
MyNimrod <behailugobeziefentaw@gmail.com>
2022-06-16 07:09:25 -0700

    HotFix2

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,20 +7,19 @@
  */
 void print_number(int n)
 {
-	int divisor = 1, i, resp;
+	unsigned int x;
 
 	if (n < 0)
 	{
-		_putchar('-');
+		_putchar('_');
 		n *= -1;
 	}
 
-	for (i = 0; n / divisor > 9; i++, divisor *= 10)
-	{
-	}
-	for (; divisor >= 1; n %= divisor, divisor /= 10)
-	{
-		resp = n / divisor;
-		_putchar('0' + resp);
-	}
+	x = n;
+
+	if (x / 10)
+		print_number(x / 10);
+
+	_putchar(x % 10 + '0');
+
 }
