a6fb79ed93c018e492a2708fbda2d6d2d4cd4df1
MyNimrod <behailugobeziefentaw@gmail.com>
2022-06-20 08:56:11 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: pointer to the string
+ * @c: holds the character to locate
+ * Description: similar to the std library strchr
+ * Return: pointer to the first location
+ */
+char *_strchr(char *s, char c);
+{
+	while (*s)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (!c ? s : NULL);
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,3 +1,4 @@
 ##0x07-pointers_arrays_strings
 0-memset.c  fills memory with a constant byte
 1-memcpy.c copies a memory area
+2-strchr.c locates a character in a string
