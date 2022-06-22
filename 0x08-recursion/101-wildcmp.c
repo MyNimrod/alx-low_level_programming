c7d30b68f60f79337008d121b397f3db1053e36b
MyNimrod <behailugobeziefentaw@gmail.com>
2022-06-22 14:47:08 -0700

    Inception. Now, before you bother telling me it's impossible...

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -21,6 +21,8 @@ char *move_past_star(char *s2)
  */
 int inception(char *s1, char *s2)
 {
+	int ret = 0;
+
 	if (*s1 == 0)
 		return (0);
 	if (*s1 == *s2)
@@ -54,7 +56,7 @@ int wildcmp(char *s1, char *s2)
 		s2 = move_past_star(s2);
 		if (!*s2)
 			return (1);
-		if (*s1 == *)
+		if (*s1 == *s2)
 			ret += wildcmp(s1 + 1, s2 + 1);
 		ret += inception(s1, s2);
 		return (!!ret);
