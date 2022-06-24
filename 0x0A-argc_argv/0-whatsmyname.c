76d8301d3469cdd17c3af2471faf5ac9363642cb
MyNimrod <behailugobeziefentaw@gmail.com>
2022-06-24 15:27:40 -0700

    It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry into the program
  * @argc: argument count
- * @argv: the array of pointers to argc 
+ * @argv: the array of pointers to argc
  * Description: prints its name, followed by a new line
  * Return: Always 0
  */
